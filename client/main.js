1e8a7742b5db657498e26ef5042300e21367e1c3
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-03-01 20:04:20 +0700

    Update main.js

--- client/main.js
+++ client/main.js
@@ -15,98 +15,13 @@ $(document).ready(function () {
       },
       success: function(response) {
         document.getElementById("response").innerHTML = response.result.replace(/\n/g, '<br />');
-        $('#download-btn').removeClass('d-none'); // Show the download button
-      }
-      
-    });
-  });
-
-  // Handle click event for the download button
-  $('#download-btn').on('click', function() {
-    const content = document.getElementById("response").innerHTML;
-    const filename = "generated_post.txt";
-    const filetype = "text/plain";
-
-    // Create a blob with the text content
-    const blob = new Blob([content], { type: filetype });
-
-    // Create a temporary URL for the blob object
-    const url = URL.createObjectURL(blob);
-
-    // Create a link element with the URL and download attributes
-    const link = document.createElement("a");
-    link.setAttribute("href", url);
-    link.setAttribute("download", filename);
-
-    // Append the link element to the document body
-    document.body.appendChild(link);
-
-    // Simulate a click event on the link element
-    link.click();
-
-    // Remove the link element from the document body
-    document.body.removeChild(link);
-
-    // Show the create post button
-    $('#create-post-btn').removeClass('d-none');
-  });
-
-  // Handle click event for the create post button
-  $('#create-post-btn').on('click', function() {
-    const content = document.getElementById("response").innerHTML;
-
-    // Send a POST request to the server to create a new post
-    $.ajax({
-      type: 'POST',
-      url: '/api/posts',
-      data: { content: content },
-      success: function(response) {
-        console.log(response);
-        alert('Post created successfully!');
-      },
-      error: function(error) {
-        console.error(error);
-        alert('Error creating post!');
+        $('#save-post').removeClass('d-none'); // Show the save post button
       }
     });
   });
-});
-$(document).ready(function () {
 
-  // ...
-
-  // Handle click event for the download button
-  $('#download-btn').on('click', function() {
-    const content = document.getElementById("response").innerHTML;
-    const filename = "generated_post.txt";
-    const filetype = "text/plain";
-
-    // Create a blob with the text content
-    const blob = new Blob([content], { type: filetype });
-
-    // Create a temporary URL for the blob object
-    const url = URL.createObjectURL(blob);
-
-    // Create a link element with the URL and download attributes
-    const link = document.createElement("a");
-    link.setAttribute("href", url);
-    link.setAttribute("download", filename);
-
-    // Append the link element to the document body
-    document.body.appendChild(link);
-
-    // Simulate a click event on the link element
-    link.click();
-
-    // Remove the link element from the document body
-    document.body.removeChild(link);
-
-    // Show the create post button
-    $('#create-post-btn').removeClass('d-none');
-  });
-
-  // Handle click event for the create post button
-  $('#create-post-btn').on('click', function() {
+  // Handle click event for the save post button
+  $('#save-post').on('click', function() {
     const content = document.getElementById("response").innerHTML;
 
     // Send a POST request to the server to create a new post
