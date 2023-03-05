65add8bf8ef43f551492cff3a130c74d5cd17472
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-03-05 17:40:59 +0700

    Update main.js

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     
     $.ajax({
       type: 'POST',
-      url: '/api/completions',
+      url: 'api/completions',
       data: {
         prompt: prompt,
         temperature: temperature,
@@ -15,31 +15,13 @@ $(document).ready(function () {
       },
       success: function(response) {
         document.getElementById("response").innerHTML = response.result.replace(/\n/g, '<br />');
-        $('#save-post').removeClass('d-none'); // Show the save post button
       }
-    });
-  });
+      
 
-  // Handle click event for the save post button
-  $('#save-post').on('click', function() {
-    const content = document.getElementById("response").innerHTML;
 
-    // Use WPAPI to create a new post
-    var wp = new WPAPI({
-      endpoint: 'https://cheekyfans.com/wp-json',
-      username: 'Jayroo',
-      password: 'llaves196214'
-    });
-    wp.posts().create({
-      title: 'Your Post Title',
-      content: content,
-      status: 'publish'
-    }).then(function(response) {
-      console.log(response);
-      alert('Post created successfully!');
-    }).catch(function(error) {
-      console.error(error);
-      alert('Error creating post!');
+
+
+      
     });
   });
 });
